d52f3be025d84653688a149cf4358de09a81d27f
Den4562 <119700990+Den4562@users.noreply.github.com>
2025-05-18 14:00:46 +0300

    Web Hook google

--- src/pages/ГЛАВНАЯ.mainPage.js
+++ src/pages/ГЛАВНАЯ.mainPage.js
@@ -1,10 +1,43 @@
-// API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import wixData from "wix-data";
+import { fetch } from "wix-fetch";
 
 $w.onReady(function () {
-    // Write your JavaScript here
+  $w("#button1").onClick(async () => {
+    let formData = {
+      firstName: $w("#input11").value,
+      lastName: $w("#input12").value,
+      email: $w("#input13").value,
+    };
 
-    // To select an element by ID use: $w('#elementID')
+    // Сохраняем в коллекцию Wix
+    await wixData
+      .insert("formUser", formData)
+      .then(() => {
+        console.log("Данные сохранены в коллекции Wix");
+      })
+      .catch((err) => {
+        console.log("Ошибка сохранения в Wix:", err);
+      });
 
-    // Click 'Preview' to run your code
+    // Отправляем данные в Google Sheets через вебхук
+    const googleWebhookUrl =
+      "https://script.google.com/macros/s/AKfycbySIe66_VgY6ZZzOavGERM1JQttmE4KlGkSc684wsKn1_v8AtTTaADat3_dEojr498teA/exec"; // Замени на URL из Apps Script
+    await fetch(googleWebhookUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Данные отправлены в Google Sheets:", data);
+        $w("#input11").value = "";
+        $w("#input12").value = "";
+        $w("#input13").value = "";
+      })
+      .catch((err) => {
+        console.log("Ошибка отправки в Google Sheets:", err);
+      });
+  });
 });
